4e8d8e4e369824235bde2bfc6b89cd32f54adaf3
RaiA133 <rai010303@gmail.com@gmail.com>
2023-12-16 15:10:30 +0700

    feature : tampilan awal kalkulator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        Scanner scanner = new Scanner();
+
+        System.out.println("\nKALKULATOR");
+        System.out.println("=========================");
+        System.out.println("1. Pertambahan ( + )");
+        System.out.println("2. Pengurangan ( - )");
+        System.out.println("3. Perkalian ( * )");
+        System.out.println("4. Pembagian ( / )");
+
     }
 }
