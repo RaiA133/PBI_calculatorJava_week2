aee5685be95968bd58c0db966ca498a640f26060
RaiA133 <rai010303@gmail.com@gmail.com>
2023-12-16 15:16:23 +0700

    feature : user input

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner();
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("\nKALKULATOR");
         System.out.println("=========================");
@@ -9,6 +10,15 @@ public class Main {
         System.out.println("2. Pengurangan ( - )");
         System.out.println("3. Perkalian ( * )");
         System.out.println("4. Pembagian ( / )");
+
+        System.out.print("\nPilih operasi (1, 2, 3, 4): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Masukkan angka pertama: ");
+        double angka1 = scanner.nextDouble();
+
+        System.out.print("Masukkan angka kedua: ");
+        double angka2 = scanner.nextDouble();
 
     }
 }
